d1afa9edd99127e9f02c7847e3b0502fafdc565e
akmot9 <avicocyprien@yahoo.com>
2023-10-03 15:55:41 +0200

    update gettable

--- src/components/GetTable.vue
+++ src/components/GetTable.vue
@@ -6,13 +6,6 @@
         <li v-for="packetInfo in packetInfoList" :key="packetInfo.id">
           <span><strong>MAC Source:</strong> {{ packetInfo.mac_source }}</span><br>
           <span><strong>MAC Destination:</strong> {{ packetInfo.mac_destination }}</span><br>
-          <span><strong>IPv4:</strong> {{ packetInfo.ethertype }}</span><br>
-          <span><strong>IP Source:</strong> {{ packetInfo.ip_source }}</span><br>
-          <span><strong>IP Destination:</strong> {{ packetInfo.ip_destination }}</span><br>
-          <span><strong>Protocol:</strong> {{ packetInfo.protocol }}</span><br>
-          <span><strong>Port Source:</strong> {{ packetInfo.port_source }}</span><br>
-          <span><strong>Port Destination:</strong> {{ packetInfo.port_destination }}</span><br>
-          <span><strong>Count:</strong> {{ packetInfo.count }}</span><br>
         </li>
       </ul>
     </div>
@@ -20,7 +13,6 @@
   
   
 <script>
-  import { ref } from 'vue';
   import { invoke } from '@tauri-apps/api/tauri';
   
   export default {
